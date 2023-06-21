3f5cfc86de85c1a4ec7cb6c799fe600eba57decc
John Subba <69120910+iceman404@users.noreply.github.com>
2023-06-21 19:27:10 +0100

    Update threading.h

--- pthreads/threading.h
+++ pthreads/threading.h
@@ -1,5 +1,12 @@
 #ifndef THREADING_H
 #define THREADING_H
+#ifdef _WIN32
+    // Include Windows-specific headers for threading
+    #include <windows.h>
+#else
+    // Include Unix-specific headers for threading
+    #include <pthread.h>
+#endif
 
 /* Declarations and function prototypes related to threading*/
 void *timer1(void *time);
