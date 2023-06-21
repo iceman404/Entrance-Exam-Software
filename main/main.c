fb9443fd49d5715780cf2b737718c801eea847c2
John Subba <69120910+iceman404@users.noreply.github.com>
2023-06-21 19:14:55 +0100

    Update main.c
    refactor 1

--- main/main.c
+++ main/main.c
@@ -1,1 +1,21 @@
+/*
+    Author: [John Subba]
+    Date: Original-[November-11, 2021]
+    Refactor Date: [June-21, 2023]
+    Description: [This is the main file of Entrance Prep Software]
+*/
 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <pthread.h>
+
+#ifdef _WIN32
+    #include <conio.h> 
+    #include <windows.h>
+#else
+    #include <unistd.h>
+    // Add any additional Unix-specific headers here
+#endif
