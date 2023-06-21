9f7fa89762adf417ac85fc1bebfa5971e12ee3ab
John Subba <69120910+iceman404@users.noreply.github.com>
2023-06-21 19:26:11 +0100

    Create threading.c

---
+++ pthreads/threading.c
@@ -0,0 +1,10 @@
+#include "threading.h"
+
+pthread_mutex_t mut1,mut2; /*for creating a mutex lock on exit flag. */
+int time_to_exit=0;   /* this global variable is the exit flag. */
+
+// Function definitions related to threading
+// ...
+
+/* Function definitions related to threading */
+
