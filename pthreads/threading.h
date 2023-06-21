453d413dbe73e8da16440808c2cc618fafd3b49b
John Subba <69120910+iceman404@users.noreply.github.com>
2023-06-21 19:30:51 +0100

    Update threading.h

--- pthreads/threading.h
+++ pthreads/threading.h
@@ -1,5 +1,6 @@
 #ifndef THREADING_H
 #define THREADING_H
+
 #ifdef _WIN32
     // Include Windows-specific headers for threading
     #include <windows.h>
@@ -7,6 +8,10 @@
     // Include Unix-specific headers for threading
     #include <pthread.h>
 #endif
+
+// Global variable declaration
+extern int time_to_exit;
+
 
 /* Declarations and function prototypes related to threading*/
 void *timer1(void *time);
