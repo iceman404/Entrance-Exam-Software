088cba22747ff5d8c14f01f7f42dfceffd1d3526
John Subba <69120910+iceman404@users.noreply.github.com>
2023-06-21 19:23:37 +0100

    Create threading.h
    threading header file created.

---
+++ pthreads/threading.h
@@ -0,0 +1,9 @@
+#ifndef THREADING_H
+#define THREADING_H
+
+/* Declarations and function prototypes related to threading*/
+void *timer1(void *time);
+void *date(d1 *);  // these are functions related to date and timer of exam.
+
+#endif // THREADING_H
+
